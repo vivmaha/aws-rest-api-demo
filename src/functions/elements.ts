b7cdb22ab2abc0f45e056d7ca91dae558c922f80
Vivek Maharajh <vivmaha@live.com>
2019-10-20 21:14:09 -0700

    Added linting and getElement(id) API

--- src/functions/elements.ts
+++ src/functions/elements.ts
@@ -1,24 +1,62 @@
-type Element = {
-    name: string;
-    atomicNumber: number;
-}
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-const elements: Element[] = [{
-        name: "Hydrogen",
-        atomicNumber: 1
-    }, {
-        name: "Helium",
-        atomicNumber: 2
-    }
+type Element = {
+  name: string;
+  atomicNumber: number;
+};
+
+const elements: Element[] = [
+  {
+    name: "Hydrogen",
+    atomicNumber: 1
+  },
+  {
+    name: "Helium",
+    atomicNumber: 2
+  }
 ];
 
-export const getElements = async () => {
-    return new Promise(
-        resolve => {
-            resolve({
-                statusCode: 200,
-                body: JSON.stringify(elements)
-            });
-        }
-    );
+export const getElements = async (): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(elements)
+  };
+};
+
+export const getElement = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const parameterName = "id";
+  const atomicNumberAsString = event.pathParameters[parameterName];
+  if (!atomicNumberAsString) {
+    return {
+      statusCode: 400,
+      body: `Missing [${parameterName}].`
+    };
+  }
+  const atomicNumber = Number(atomicNumberAsString);
+  if (
+    Number.isNaN(atomicNumber) ||
+    atomicNumber !== Math.floor(atomicNumber) ||
+    atomicNumber < 1
+  ) {
+    return {
+      statusCode: 500,
+      body: `[${parameterName}] has to be a positive integer.`
+    };
+  }
+
+  const element = elements.find(
+    element => element.atomicNumber === atomicNumber
+  );
+  if (!element) {
+    return {
+      statusCode: 404,
+      body: "Element not found."
+    };
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(element)
+  };
 };
